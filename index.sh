8edf5c7edd6d2c1f53b481449b6bf9dd81be06a0
Ling Z <zhangling.co@gmail.com>
2019-06-17 16:42:23 +0800

    feat: 文件以markdown表格的形式输出到dist.md

--- index.sh
+++ index.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+# git 代码行数统计（以人为维度）
+
+rm -rf "dist.md"
 gitProjects=(
-	'http://10.211.62.41:82/c4i-microservices/aws-c4i-bi-service.git'
-	'http://10.211.62.41:82/c4i-microservices/aws-c4i-bi-service-api.git'
+	'git@github.com:zhanglingco/Blog.git'
+	'git@github.com:zhanglingco/coder-interview.git'
 );
 
 for(( i=0;i<${#gitProjects[@]};i++)) do
@@ -9,12 +13,18 @@ for(( i=0;i<${#gitProjects[@]};i++)) do
 
 	dirName=$(basename ${gitProjects[i]} | awk -F "." '{print $1}')
 	echo ${gitProjects[i]};
+	printf 'user name | added lines | removed lines | total lines\n---- | --- | --- | ---\n'
+
 	cd $dirName;
 	git log --format='%aN' | sort -u |
 	while read name;
-	do echo -en "$name\t";
-		git log --author="$name" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -;
+	do
+		echo -en "$name | ";
+		git log --author="$name" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "%s | %s | %s\n", add, subs, loc }' -;
 	done;
+	printf "\n";
 
-	cd ..
-done;
+	# 重置&清理
+	cd ..;
+	rm -rf $dirName;
+done >> "dist.md";
